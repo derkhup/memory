f244ada8afebb2fccd5b860f1c32913e8e5b437a
Your Name <you@example.com>
2017-11-09 15:11:08 +0100

    scptkkkkk    eend op scherm

--- index.php
+++ index.php
@@ -3,13 +3,19 @@
     <head>
 
         <script>
-            function verander() {
-                var duckvar;
+            function verander(teller) {
 
-                 document.getElementById("duck").src = "streep.jpg";
-                alert(duckvar);
-               
-                style.color = "red";
+                var duckvar;
+                if (teller = 1) {
+                    document.getElementById("duck1").src = "streep.jpg";
+                } 
+                else {
+                    alert(teller);
+                    document.getElementById("duck2").src = "streep.jpg";
+                }
+
+
+//                style.color = "red";
 
 
 
@@ -22,17 +28,18 @@
     <body>
         <table> <tr>
                 <td 
-                    <p >   <img src="eend.png" width="210" height="210" id="duck" /> </p>
-                    <button onclick = "verander()"  >  qaz </button>
-
-                </td> </tr>
-
-<!----        <img onclick="verander()" src="eend.png" id=duck width="210" height="210" alt="eend"  loll/>
-
-
-
-            <?php
-            // put your code here
-            ?>
-</body>
-</html>
+                    <p>  <img onclick = "verander(1)" src="zwart.png" width="210" height="210" id="duck1" /> </p>
+                    <p>  <img onclick = "verander(2)" src="zwart.png" width="210" height="210" id="duck2" /> </p>
+                    <!-----                    <button onclick = "verander()"  >  qaz </button>
+                    
+                                    </td> </tr>
+                    
+                    <!----        <img onclick="verander()" src="eend.png" id=duck width="210" height="210" alt="eend"  loll/>
+                    
+                    
+                    
+                    <?php
+                    // put your code here
+                    ?>
+                    </body>
+                    </html>
