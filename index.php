a5cae0acce86b94c3c840a529a33f01c736613d5
Your Name <you@example.com>
2017-11-09 13:22:06 +0100

    eend op scherm

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<html>
+    <head>
+       
+        <title>memory</title>
+    </head>
+    <body>
+       
+        <img src="eend.png" width="210" height="210" alt="eend"/>
+        <img src="eend.png" width="210" height="210" alt="eend"/>
+
+
+ <?php
+        // put your code here
+        ?>
+    </body>
+</html>
